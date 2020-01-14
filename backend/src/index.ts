cbc6fdf9005b72f7cd1dd1f90e147f8cf911404e
LeeNamHeun <namheun@naver.com>
2020-01-14 12:34:18 +0900

    Create graphql types

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import { GraphQLServer } from "graphql-yoga";
 import connnection from "./ormConfig";
+import schema from './schema';
 
 
+/*
+스키마(파일)로 전환 
 //인자값과 리턴되는 값의 타입을 지정 ---스키마 
 const typeDefs=`
     type Query{
@@ -15,11 +18,12 @@ const resolvers = {
     sayHello:() => "Hi there :0)"
     }
 };
-
-    
 const server = new GraphQLServer({typeDefs, resolvers});
-//server.start({ port: 4000 }, ()=>console.log("My first GraphQL Server is on localhost:4000"));
 
+*/
+
+const server = new GraphQLServer({schema});
+    
 connnection.then(()=>(
     server.start(()=> console.log("My First Graphql server is running on location"))
 ));
